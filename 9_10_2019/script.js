1cd5fbddbd368c098025f1026cd45f36867ffea7
Jason Gilbert <51034552+gilbertjusmc@users.noreply.github.com>
2019-09-15 09:50:11 -0400

    added 9-10-19

---
+++ 9_10_2019/script.js
@@ -0,0 +1,5 @@
+// Using Javascript, Grab the contents within an HTML div, make it a string and assign it to a variable
+
+htmlTextPull = document.getElementById('content').innerText
+console.log(htmlTextPull)
+
